ec30bfe6582add178ca5fabcd0b187040f3eb4a7
Zhan <zhanarbek.a7@gmail.com>
2025-04-11 20:22:03 -0500

    Ex12

--- src/Arrays/Ex12Permutation.java
+++ src/Arrays/Ex12Permutation.java
@@ -3,12 +3,12 @@ package Arrays;
 public class Ex12Permutation {
     public static void main(String[] args) {
         int[] array1 = {1,2,5};
-        int[] array2 = {5,1,2};
-        System.out.println(permutation(array1, array2));
+        int[] array2 = {5,1,3};
+        System.out.println(permutationFast(array1, array2));
     }
 
-    // O(n)
-    public static boolean permutation(int[] array1, int[] array2){
+    // O(n^2)
+    public static boolean permutationSlow(int[] array1, int[] array2){
         int counter = 0;
         boolean permutation = true;
         if(array1.length != array2.length){
@@ -28,5 +28,22 @@ public class Ex12Permutation {
         }
 
         return permutation;
+    }
+
+    // O (n)
+    public static boolean permutationFast(int[] array1, int[] array2){
+        if(array1.length != array2.length){
+            return false;
+        }
+        int sum1 = 0, sum2 = 0, mult1 = 1, mult2 = 1;
+
+        for(int i = 0; i < array1.length; i++){
+            sum1 += array1[i];
+            sum2 += array2[i];
+            mult1*=array1[i];
+            mult2*=array2[i];
+        }
+
+        return sum1 == sum2 && mult1 == mult2;
     }
 }
