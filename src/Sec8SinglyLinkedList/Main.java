c05fecd9c9ad01a724dc220a9f032322f09f3124
Zhan <zhanarbek.a7@gmail.com>
2025-05-28 02:32:07 -0500

    Singly Linked List Deletion methods implementation done

--- src/Sec8SinglyLinkedList/Main.java
+++ src/Sec8SinglyLinkedList/Main.java
@@ -8,10 +8,22 @@ public class Main {
         sLL.createSinglyLinkedList(5);
         sLL.insertInLinkedList(6,1);
         sLL.insertInLinkedList(7,3);
-        sLL.insertInLinkedList(8,4);
-        sLL.insertInLinkedList(9,0);
+        sLL.addLast(12);
+        sLL.addLast(99);
         sLL.traverseSinglyLinkedList();
-        System.out.println(sLL.searchNode(66));
+        sLL.deleteNode(4);
 
+//        System.out.println(sLL.size);
+//        sLL.traverseSinglyLinkedList();
+//        System.out.println(sLL.searchNode(66));
+//        System.out.println(sLL.deleteFirstNode());
+
+//        sLL.deleteLastNode();
+//        System.out.println(sLL.head);
+//        System.out.println(sLL.tail);
+//        System.out.println(sLL.size);
+        sLL.traverseSinglyLinkedList();
+        sLL.deleteSinglyLinkedList();
+        sLL.traverseSinglyLinkedList();
     }
 }
--- src/Sec8SinglyLinkedList/SinglyLinkedList.java
+++ src/Sec8SinglyLinkedList/SinglyLinkedList.java
@@ -5,31 +5,29 @@ public class SinglyLinkedList {
     public Node tail;
     public int size;
 
-    public Node createSinglyLinkedList(int nodeValue){
+    public Node createSinglyLinkedList(int nodeValue) {
         Node node = new Node();
         node.next = null;
         node.value = nodeValue;
         head = node;
         tail = node;
-        size=1;
+        size = 1;
         return head;
     }
 
-    public void insertInLinkedList(int nodeValue, int location){
+    public void insertInLinkedList(int nodeValue, int location) {
         Node node = new Node();
         node.value = nodeValue;
-        if(head==null){
+        if (head == null) {
             createSinglyLinkedList(nodeValue);
-        }
-        else if(location >= size){
+        } else if (location >= size) {
             node.next = null;
             tail.next = node;
             tail = node;
-        }
-        else{
+        } else {
             Node tempNode = head;
             int index = 0;
-            while(index < location -1 ){
+            while (index < location - 1) {
                 tempNode = tempNode.next;
                 index++;
             }
@@ -40,26 +38,24 @@ public class SinglyLinkedList {
         size++;
     }
 
-    public void addFirst(int nodeValue){
+    public void addFirst(int nodeValue) {
         Node node = new Node();
         node.value = nodeValue;
-        if(head==null){
+        if (head == null) {
             createSinglyLinkedList(nodeValue);
-        }
-        else{
+        } else {
             node.next = head;
             head = node;
         }
         size++;
     }
 
-    public void addLast(int nodeValue){
+    public void addLast(int nodeValue) {
         Node node = new Node();
         node.value = nodeValue;
-        if(head==null){
+        if (head == null) {
             createSinglyLinkedList(nodeValue);
-        }
-        else{
+        } else {
             node.next = null;
             tail.next = node;
             tail = node;
@@ -67,24 +63,106 @@ public class SinglyLinkedList {
         size++;
     }
 
-    public void traverseSinglyLinkedList(){
-        if(head!=null){
+    public void traverseSinglyLinkedList() {
+        if (head != null) {
             Node head = this.head;
-            while(head!=null){
+            while (head != null) {
                 System.out.print(head.value + " ");
                 head = head.next;
             }
             System.out.println();
         }
+        else{
+            System.out.println("SLL doesn't exist!");
+        }
     }
 
-    public boolean searchNode(int val){
+    public boolean searchNode(int val) {
         Node temp = head;
-        while(temp!=null){
-            if(temp.value == val) return true;
+        while (temp != null) {
+            if (temp.value == val) return true;
             temp = temp.next;
         }
         return false;
+    }
+
+    public boolean deleteFirstNode() {
+        if (head == null) {
+            System.out.println("The SLL doesn't exist");
+            return false;
+        } else {
+            head = head.next;
+            size--;
+            if (size == 0) {
+                tail = head;
+            }
+            return true;
+        }
+    }
+
+    public boolean deleteLastNode() {
+        if (head == null) {
+            System.out.println("The SLL doesn't exist");
+            return false;
+        } else {
+            Node temp = head;
+            Node secondToLast = null;
+            for (int i = 0; i < size; i++) {
+                if (temp.next == null) {
+                    break;
+                }
+                secondToLast = temp;
+                temp = temp.next;
+            }
+            if (secondToLast != null) {
+                secondToLast.next = null;
+            }
+            tail = secondToLast;
+            size--;
+            if (size == 0) {
+                head = tail;
+            }
+            return true;
+        }
+    }
+
+    public boolean deleteNode(int location) {
+        if (head == null || location > size) {
+            return false;
+        } else if (location == 0) {
+            return deleteFirstNode();
+        }
+        else if(location==size-1){
+            return deleteLastNode();
+        }
+        else {
+            Node temp = head;
+            Node secondToLast = null;
+            for (int i = 0; i < location; i++) {
+                secondToLast = temp;
+                if (temp.next == null) {
+                    break;
+                }
+                temp = temp.next;
+            }
+            if (secondToLast != null) {
+                secondToLast.next = temp.next;
+                if (tail == temp) {
+                    tail = secondToLast;
+                }
+            }
+            size--;
+            if (size == 0) {
+                head = tail;
+            }
+            return true;
+        }
+    }
+
+    public void deleteSinglyLinkedList(){
+        head = null;
+        tail = null;
+        System.out.println("Singly Linked List has been deleted successfully");
     }
 
 }
